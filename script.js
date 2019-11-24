5720d1967991e8d8d54e968b70fe9b48799b8ec5
ArtemiyIvanov <ivanov_artemiy_pavlovich@mail.ru>
2019-11-24 21:58:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+$('.sl').slick({
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
